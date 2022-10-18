fd49e8cdba1f23fafcd2d4e70ffe019b8099459d
alvaro270 <alvarogamer270@gmail.com>
2022-10-17 20:57:52 -0500

    Agregar archivos de proyecto.

---
+++ State/Encendido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    public class Encendido : Estado
+    {
+        public override void ControlarEstado(Switch sw)
+        {
+            sw.DefinirEstado(new Apagado());
+        }
+        public override string Describir()
+        {
+            return "Switch encendido";
+        }
+    }
+}
